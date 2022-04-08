c0f43757a6a85bd56ad721acfee480f735eb676b
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2022-04-09 00:28:18 +0530

    resolve error

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const path = require('path')
 const port = process.env.PORT || 5000
 
 connectDB()
